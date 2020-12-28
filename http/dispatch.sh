0b88d72214d1a28020e920dc8f333e1c788221c0
Robert Joseph Cox Jr <robert@studiorclv.com>
2020-12-28 01:21:38 -0800

    updated the dispatcher to support new response types, and de-duplicate some code

--- http/dispatch.sh
+++ http/dispatch.sh
@@ -6,20 +6,51 @@ function dispatch {
 }
 
 ##
-# Dispatches a successful response
+# Dispatches a request
 ##
-function dispatch.success {
-    local CONTENTS=${1:-\{\"status\": \"success\"\}}
-    local CONTENT_TYPE="${2:-application/json}"
-
+function dispatch.request {
+    local STATUS_CODE=${1}
+    local CONTENTS=${2}
+    local CONTENT_TYPE=${3}
+    
     echo "\
-HTTP/1.1 200 OK
+HTTP/1.1 ${STATUS_CODE}
 Connection: keep-alive
 Content-Length: $((${#CONTENTS} + 2))
 Content-Type: ${CONTENT_TYPE}
 
 ${CONTENTS}
 "
+}
+
+##
+# Dispatches a successful response
+##
+function dispatch.success {
+    local CONTENTS=${1:-\{\"status\": \"success\"\}}
+    local CONTENT_TYPE="${2:-application/json}"
+
+    dispatch.request "200 OK" ${CONTENTS} ${CONTENT_TYPE}
+}
+
+##
+# Dispathces a bad request response
+##
+function dispatch.badRequest {
+    local CONTENTS=${1:-\{\"status\": \"bad request\"\}}
+    local CONTENT_TYPE="${2:-application/json}"
+
+    dispatch.request "400 BAD REQUEST" ${CONTENTS} ${CONTENT_TYPE}
+}
+
+##
+# Dispathces an unauthorized response
+##
+function dispatch.unauthorized {
+    local CONTENTS=${1:-\{\"status\": \"bad request\"\}}
+    local CONTENT_TYPE="${2:-application/json}"
+
+    dispatch.request "401 UNAUTHORIZED" ${CONTENTS} ${CONTENT_TYPE}
 }
 
 ##
@@ -29,14 +60,7 @@ function dispatch.forbidden {
     local CONTENTS=${1:-\{\"status\": \"forbidden\"\}}
     local CONTENT_TYPE="${2:-application/json}"
 
-    echo "\
-HTTP/1.1 200 OK
-Connection: keep-alive
-Content-Length: $((${#CONTENTS} + 2))
-Content-Type: ${CONTENT_TYPE}
-
-${CONTENTS}
-"
+    dispatch.request "403 FORBIDDEN" ${CONTENTS} ${CONTENT_TYPE}
 }
 
 ##
@@ -46,12 +70,5 @@ function dispatch.notFound {
     local CONTENTS=${1:-\{\"status\": \"not found\"\}}
     local CONTENT_TYPE=${2:-application/json}
 
-    echo "\
-HTTP/1.1 404 NOT FOUND
-Connection: keep-alive
-Content-Length: $((${#CONTENTS} + 2))
-Content-Type: ${CONTENT_TYPE}
-
-${CONTENTS}
-"
+    dispatch.request "404 NOT FOUND" ${CONTENTS} ${CONTENT_TYPE}
 }
